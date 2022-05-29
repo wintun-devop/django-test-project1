44c3be04dc7a3a396b43b38c4e8f9e16577ed247
Win Tun <wintun.hlaing@outlook.com>
2022-05-29 10:01:22 +0630

    v1.002

--- sampleapp1/itshop/urls.py
+++ sampleapp1/itshop/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from itshop import views
+from . import views
 
 urlpatterns = [
     path("",views.index,name="home-page"),
--- sampleapp1/itshop/views.py
+++ sampleapp1/itshop/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.http import HttpResponseRedirect
 
-# Create your views here.
 
-def index
+
+def index(request):
+    return render()
